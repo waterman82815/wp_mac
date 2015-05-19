d9fca9030b849b0e14d73494ecaf29dd923bea83
waterman82815 <waterman82815@gmail.com>
2015-05-19 13:47:43 +0800

    modify
    modify

--- weather/weather.js
+++ weather/weather.js
@@ -200,4 +200,59 @@ var callbackFunction_ph = function(data) {
 
 
 
+<!-- 
+// <script src="weather.js"></script>
+
+
+
+
+// <script src="https://query.yahooapis.com/v1/public/yql?q=select * from weather.forecast where woeid = 2306179 &format=json&callback=callbackFunction_tp"></script>
+
+// <script src="https://query.yahooapis.com/v1/public/yql?q=select * from weather.forecast where woeid = 2306214 &format=json&callback=callbackFunction_np"></script>
+
+// <script src="https://query.yahooapis.com/v1/public/yql?q=select * from weather.forecast where woeid = 2306181 &format=json&callback=callbackFunction_tc"></script>
+
+// <script src="https://query.yahooapis.com/v1/public/yql?q=select * from weather.forecast where woeid = 2306182 &format=json&callback=callbackFunction_tn"></script>
+
+// <script src="https://query.yahooapis.com/v1/public/yql?q=select * from weather.forecast where woeid = 2306180 &format=json&callback=callbackFunction_ks"></script>
+
+// <script src="https://query.yahooapis.com/v1/public/yql?q=select * from weather.forecast where woeid = 2306188 &format=json&callback=callbackFunction_gl"></script>
+
+// <script src="https://query.yahooapis.com/v1/public/yql?q=select * from weather.forecast where woeid = 2306184 &format=json&callback=callbackFunction_tu"></script>
+
+// <script src="https://query.yahooapis.com/v1/public/yql?q=select * from weather.forecast where woeid = 2306185 &format=json&callback=callbackFunction_hs"></script>
+
+// <script src="https://query.yahooapis.com/v1/public/yql?q=select * from weather.forecast where woeid = 2306185 &format=json&callback=callbackFunction_hss"></script>
+
+// <script src="https://query.yahooapis.com/v1/public/yql?q=select * from weather.forecast where woeid = 2306229 &format=json&callback=callbackFunction_ml"></script>
+
+// <script src="https://query.yahooapis.com/v1/public/yql?q=select * from weather.forecast where woeid = 2306201 &format=json&callback=callbackFunction_ch"></script>
+
+// <script src="https://query.yahooapis.com/v1/public/yql?q=select * from weather.forecast where woeid = 2306204 &format=json&callback=callbackFunction_nt"></script>
+
+// <script src="https://query.yahooapis.com/v1/public/yql?q=select * from weather.forecast where woeid = 2306250 &format=json&callback=callbackFunction_ul"></script>
+
+// <script src="https://query.yahooapis.com/v1/public/yql?q=select * from weather.forecast where woeid = 2306206 &format=json&callback=callbackFunction_ci"></script>
+
+// <script src="https://query.yahooapis.com/v1/public/yql?q=select * from weather.forecast where woeid = 2306206 &format=json&callback=callbackFunction_cii"></script>
+
+// <script src="https://query.yahooapis.com/v1/public/yql?q=select * from weather.forecast where woeid = 2306225 &format=json&callback=callbackFunction_pt"></script>
+
+// <script src="https://query.yahooapis.com/v1/public/yql?q=select * from weather.forecast where woeid = 2306243 &format=json&callback=callbackFunction_il"></script>
+
+// <script src="https://query.yahooapis.com/v1/public/yql?q=select * from weather.forecast where woeid = 2306187 &format=json&callback=callbackFunction_hl"></script>
+
+// <script src="https://query.yahooapis.com/v1/public/yql?q=select * from weather.forecast where woeid = 2306190 &format=json&callback=callbackFunction_tt"></script>
+
+// <script src="https://query.yahooapis.com/v1/public/yql?q=select * from weather.forecast where woeid = 22695856 &format=json&callback=callbackFunction_ph"></script>
+
+// <script src="https://query.yahooapis.com/v1/public/yql?q=select * from weather.forecast where woeid = 28760735 &format=json&callback=callbackFunction_cm"></script>
+
+// <script src="https://query.yahooapis.com/v1/public/yql?q=select * from weather.forecast where woeid =  90413595 &format=json&callback=callbackFunction_lc"></script>
+
+
+
+ -->
+
+
 
